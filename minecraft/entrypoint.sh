10de67137750773ef3e6930f344d177016e80646
Anonymous <satoshin@gmx.com>
2022-01-13 22:46:26 -0500

    more updates

--- minecraft/entrypoint.sh
+++ minecraft/entrypoint.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 set -eE -o pipefail
 
+export BANANODE_IP=$(getent hosts bananode | awk '{ print $1 }')
+
+yglu config.yglu > config.yml
+
+#cp BananoEconomy-1.0-SNAPSHOT.jar ./plugins/BananoEconomy-1.0-SNAPSHOT.jar
+#cp craftbukkit-1.18.jar ./plugins/craftbukkit-1.18.jar
+
+source /start
