0e5c1d968d7831e87aa8d2534f77e0dc92480a5a
Anonymous <satoshin@gmx.com>
2022-01-14 22:55:13 -0500

    mongo updates

--- mongo/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -eE -o pipefail
-
-mongo $MONGO_INITDB_DATABASE mongo-init.js
-
--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -1,2 +1,2 @@
-use process.env.MONGO_INITDB_DATABASE
-db.createCollection(process.env.MONGO_INIT_COLLECTION)
+use BananoCraft;
+db.createCollection("users");
