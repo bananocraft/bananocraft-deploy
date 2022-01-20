d239ed50d0821035298b4ff5f483b566b7844df9
Anonymous <satoshin@gmx.com>
2022-01-19 21:04:57 -0500

    more improvments

--- minecraft/check_backup.sh
+++ minecraft/check_backup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "seeing if any previous snapshots of this server are stored and checking their validity"
+echo "seeing if a previous snapshots of server $SERVER_NAME are stored and checking their validity"
 RESTIC_PASSWORD=$RESTIC_PASSWORD restic --repo rclone:bananocraft:bananocraftbackups/${SERVER_NAME}-backup check
+
+echo "these are the versions backed up and available for restore for server $SERVER_NAME"
+RESTIC_PASSWORD=$RESTIC_PASSWORD restic --repo rclone:bananocraft:bananocraftbackups/${SERVER_NAME}-backup snapshots
 
 echo "done checking status of restore"
--- minecraft/restore.sh
+++ minecraft/restore.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-echo "restoring from last valid backup"
+echo "restoring from backup $RESTORE_VERSION"
 RESTIC_PASSWORD=$RESTIC_PASSWORD | restic --repo rclone:bananocraft:bananocraftbackups/${SERVER_NAME}-backup restore $RESTORE_VERSION --target /data
 
 touch /data/.restored
-
-reboot
