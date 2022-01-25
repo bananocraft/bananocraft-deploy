151ec35d3436ae5a39b6ad87a6267d7125504cfb
Anonymous <satoshin@gmx.com>
2022-01-24 22:18:36 -0500

    geyser and restores

--- minecraft/restore.sh
+++ minecraft/restore.sh
@@ -2,6 +2,13 @@
 
 echo "restoring from backup $RESTORE_VERSION"
 RESTIC_PASSWORD=$RESTIC_PASSWORD restic --no-lock --repo rclone:bananocraft:bananocraftbackups/${SERVER_NAME}-backup unlock || true
-RESTIC_PASSWORD=$RESTIC_PASSWORD | restic --repo rclone:bananocraft:bananocraftbackups/${SERVER_NAME}-backup restore $RESTORE_VERSION --target /data
+mkdir /restorefiles
+mkdir /restore
+RESTIC_PASSWORD=$RESTIC_PASSWORD restic --repo rclone:bananocraft:bananocraftbackups/${SERVER_NAME}-backup restore $RESTORE_VERSION --target /restorefiles
+sudo chown -R 1000:1000 /restorefiles
+sudo chown -R 1000:1000 /data
+zip -r /restore/restore.zip /restorefiles
+rm -rf /restorefiles
+export GENERIC_PACK=/restore/restore.zip
 
 touch /data/.restored
