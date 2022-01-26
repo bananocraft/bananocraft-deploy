479b599a3417b96b8e246467b7854cbd9e2fd477
Anonymous <satoshin@gmx.com>
2022-01-26 13:23:42 -0500

    uncomment restore

--- minecraft/restore.sh
+++ minecraft/restore.sh
@@ -5,9 +5,9 @@ RESTIC_PASSWORD=$RESTIC_PASSWORD restic --no-lock --repo rclone:bananocraft:bana
 mkdir /restorefiles
 mkdir /restore
 RESTIC_PASSWORD=$RESTIC_PASSWORD restic --repo rclone:bananocraft:bananocraftbackups/${SERVER_NAME}-backup restore $RESTORE_VERSION --target /restorefiles
+yglu /scripts/config.yglu > /restorefiles/data/plugins/BananoEconomy/config.yml
 sudo chown -R 1000:1000 /restorefiles
 sudo chown -R 1000:1000 /data
-yglu /scripts/config.yglu > /restorefiles/plugins/BananoEconomy/config.yml
 zip -r /restore/restore.zip /restorefiles
 rm -rf /restorefiles
 export GENERIC_PACK=/restore/restore.zip
