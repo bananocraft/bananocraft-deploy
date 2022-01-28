7b3a4f138eeac2d7459d9d1048f910ec142c256c
Anonymous <satoshin@gmx.com>
2022-01-28 13:15:04 -0500

    get rid of false alerts

--- minecraft/backup.sh
+++ minecraft/backup.sh
@@ -16,14 +16,14 @@ while true; do
   echo "running restic"
   RESTIC_PASSWORD=$RESTIC_PASSWORD restic --repo rclone:bananocraft:bananocraftbackups/${SERVER_NAME}-backup init || true
   RESTIC_PASSWORD=$RESTIC_PASSWORD restic --repo rclone:bananocraft:bananocraftbackups/${SERVER_NAME}-backup backup /data || EXIT_CODE=$?
-  if [ $EXIT_CODE -eq 0 ]; then
-    /usr/local/bin/mcrcon -H localhost -P $RCON_PORT -p "$RCON_PASSWORD" "say Backup complete!"
-    curl -H "Content-Type: application/json" -d "{\"username\": \"bananocraftbot\", \"content\": \"Backup complete for ${SERVER_NAME}\"}" $DISCORD_WEBHOOK_URL
-    /bin/echo "Backup complete!"
-  else
+  if [ $EXIT_CODE -ne 0 ]; then
     /usr/local/bin/mcrcon -H localhost -P $RCON_PORT -p "$RCON_PASSWORD" "Backups not complete. Notify mconstant#4986 on Discord to fix backups for ${SERVER_NAME}. Attempting to send discord webhook automatically."
     curl -H "Content-Type: application/json" -d "{\"username\": \"bananocraftbot\", \"content\": \"Backups on bananocraft server ${SERVER_NAME} need to be fixed\"}" $DISCORD_WEBHOOK_URL
     /bin/echo "Backups not complete!"
+  else
+    /usr/local/bin/mcrcon -H localhost -P $RCON_PORT -p "$RCON_PASSWORD" "say Backup complete!"
+    curl -H "Content-Type: application/json" -d "{\"username\": \"bananocraftbot\", \"content\": \"Backup complete for ${SERVER_NAME}\"}" $DISCORD_WEBHOOK_URL
+    /bin/echo "Backup complete!"
   fi
   RESTIC_PASSWORD=$RESTIC_PASSWORD restic --repo rclone:bananocraft:bananocraftbackups/${SERVER_NAME}-backup forget --keep-hourly 48 --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 75 --prune
   RESTIC_PASSWORD=$RESTIC_PASSWORD restic --repo rclone:bananocraft:bananocraftbackups/${SERVER_NAME}-backup snapshots > available_snapshots.txt
