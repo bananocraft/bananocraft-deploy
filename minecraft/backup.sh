4a7cfd448defeb4190a37f19725e25f0a92d6f32
Anonymous <satoshin@gmx.com>
2022-01-28 08:14:50 -0500

    updates to backup script

--- minecraft/backup.sh
+++ minecraft/backup.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
 while true; do
-    sleep $(($BACKUP_MINUTE_INTERVAL * 60))
-    rm /data/.restored || true
+  sleep $(($BACKUP_MINUTE_INTERVAL * 60))
+  rm /data/.restored || true
 
-    /bin/echo "Starting backup..."
+  /bin/echo "Starting backup..."
 
-    /usr/local/bin/mcrcon -H localhost -P $RCON_PORT -p "$RCON_PASSWORD" "say Backing up server."
-    curl -H "Content-Type: application/json" -d "{\"username\": \"bananocraftbot\", \"content\": \"Backing up bananocraft server ${SERVER_NAME}\"}" $DISCORD_WEBHOOK_URL
-    /usr/local/bin/mcrcon -H localhost -P $RCON_PORT -p "$RCON_PASSWORD" save-off
-    /usr/local/bin/mcrcon -H localhost -P $RCON_PORT -p "$RCON_PASSWORD" save-all
+  /usr/local/bin/mcrcon -H localhost -P $RCON_PORT -p "$RCON_PASSWORD" "say Backing up server."
+  curl -H "Content-Type: application/json" -d "{\"username\": \"bananocraftbot\", \"content\": \"Backing up bananocraft server ${SERVER_NAME}\"}" $DISCORD_WEBHOOK_URL
+  /usr/local/bin/mcrcon -H localhost -P $RCON_PORT -p "$RCON_PASSWORD" save-off
+  /usr/local/bin/mcrcon -H localhost -P $RCON_PORT -p "$RCON_PASSWORD" save-all
 
-    sleep 10
+  sleep 10
 
-    echo "running restic"
-    RESTIC_PASSWORD=$RESTIC_PASSWORD restic --repo rclone:bananocraft:bananocraftbackups/${SERVER_NAME}-backup init || true
-    RESTIC_PASSWORD=$RESTIC_PASSWORD restic --repo rclone:bananocraft:bananocraftbackups/${SERVER_NAME}-backup backup /data
-    RESTIC_PASSWORD=$RESTIC_PASSWORD restic --repo rclone:bananocraft:bananocraftbackups/${SERVER_NAME}-backup forget --keep-hourly 48 --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 75 --prune
-    if [ $? -eq 0 ]; then
-      /usr/local/bin/mcrcon -H localhost -P $RCON_PORT -p "$RCON_PASSWORD" "say Backup complete!"
-      curl -H "Content-Type: application/json" -d "{\"username\": \"bananocraftbot\", \"content\": \"Backup complete for ${SERVER_NAME}\"}" $DISCORD_WEBHOOK_URL
-      /bin/echo "Backup complete!"
-    else
-      /usr/local/bin/mcrcon -H localhost -P $RCON_PORT -p "$RCON_PASSWORD" "Backups not complete. Notify mconstant#4986 to fix backups for ${SERVER_NAME}. Attempting to send discord webhook automatically."
-      curl -H "Content-Type: application/json" -d "{\"username\": \"bananocraftbot\", \"content\": \"Backups on bananocraft server ${SERVER_NAME} need to be fixed\"}" $DISCORD_WEBHOOK_URL
-      /bin/echo "Backups not complete!"
-    fi
-    RESTIC_PASSWORD=$RESTIC_PASSWORD restic --repo rclone:bananocraft:bananocraftbackups/${SERVER_NAME}-backup snapshots > available_snapshots.txt
-    curl -F "payload_json={\"username\": \"bananocraftbot\", \"content\": \"available snapshots for $SERVER_NAME\"}" -F "file1=@available_snapshots.txt" $DISCORD_WEBHOOK_URL
-    /usr/local/bin/mcrcon -H localhost -P $RCON_PORT -p "$RCON_PASSWORD" "check #backupbot channel on bananocraft.cc Discord for a log of available snapshots"
+  echo "running restic"
+  RESTIC_PASSWORD=$RESTIC_PASSWORD restic --repo rclone:bananocraft:bananocraftbackups/${SERVER_NAME}-backup init || true
+  RESTIC_PASSWORD=$RESTIC_PASSWORD restic --repo rclone:bananocraft:bananocraftbackups/${SERVER_NAME}-backup backup /data || EXIT_CODE=$?
+  if [ $EXIT_CODE -eq 0 ]; then
+    /usr/local/bin/mcrcon -H localhost -P $RCON_PORT -p "$RCON_PASSWORD" "say Backup complete!"
+    curl -H "Content-Type: application/json" -d "{\"username\": \"bananocraftbot\", \"content\": \"Backup complete for ${SERVER_NAME}\"}" $DISCORD_WEBHOOK_URL
+    /bin/echo "Backup complete!"
+  else
+    /usr/local/bin/mcrcon -H localhost -P $RCON_PORT -p "$RCON_PASSWORD" "Backups not complete. Notify mconstant#4986 on Discord to fix backups for ${SERVER_NAME}. Attempting to send discord webhook automatically."
+    curl -H "Content-Type: application/json" -d "{\"username\": \"bananocraftbot\", \"content\": \"Backups on bananocraft server ${SERVER_NAME} need to be fixed\"}" $DISCORD_WEBHOOK_URL
+    /bin/echo "Backups not complete!"
+  fi
+  RESTIC_PASSWORD=$RESTIC_PASSWORD restic --repo rclone:bananocraft:bananocraftbackups/${SERVER_NAME}-backup forget --keep-hourly 48 --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 75 --prune
+  RESTIC_PASSWORD=$RESTIC_PASSWORD restic --repo rclone:bananocraft:bananocraftbackups/${SERVER_NAME}-backup snapshots > available_snapshots.txt
+  curl -F "payload_json={\"username\": \"bananocraftbot\", \"content\": \"available snapshots for $SERVER_NAME\"}" -F "file1=@available_snapshots.txt" $DISCORD_WEBHOOK_URL
+  /usr/local/bin/mcrcon -H localhost -P $RCON_PORT -p "$RCON_PASSWORD" "check #backupbot channel on bananocraft.cc Discord for a log of available snapshots"
 
-    /usr/local/bin/mcrcon -H localhost -P $RCON_PORT -p "$RCON_PASSWORD" save-on
+  /usr/local/bin/mcrcon -H localhost -P $RCON_PORT -p "$RCON_PASSWORD" save-on
 
 done
