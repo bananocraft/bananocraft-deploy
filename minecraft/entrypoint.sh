691b6469a62c2e53bf467833a3866e20eb802e4c
Miner82 <algebruikt82@gmail.com>
2023-01-06 14:51:38 +0100

    Dev81: Plugin updates
    - BananoMiner v1.0.26
    - BananoSuite v1.0.13
    - Add NoEncryption
    - Add BananoChests v1.0.0
    - Add server icon (initial)

--- minecraft/entrypoint.sh
+++ minecraft/entrypoint.sh
@@ -3,9 +3,10 @@ set -eE -o pipefail
 
 yglu config.yglu > config.yml
 yglu bananominerconfig.yglu > bananominerconfig.yml
+yglu bananosuiteconfig.yglu > bananosuiteconfig.yml
+yglu bananochestsconfig.yglu > bananochestsconfig.yml
 yglu geyserconfig.yglu > geyserconfig.yml
 yglu petshopconfig.yglu > petshopconfig.yml
-yglu bananosuiteconfig.yglu > bananosuiteconfig.yml
 yglu pvptoggle.yglu > pvptoggle.yml
 
 mkdir -p plugins/BananoEconomy
@@ -18,9 +19,10 @@ mkdir -p plugins/PvPToggle
 
 cp config.yml ./plugins/BananoEconomy/config.yml
 cp bananominerconfig.yml ./plugins/BananoMiner/config.yml
+cp bananosuiteconfig.yml ./plugins/BananoSuite/config.yml
+cp bananochestsconfig.yml ./plugins/BananoChests/config.yml
 cp geyserconfig.yml ./plugins/Geyser-Spigot/config.yml
 cp petshopconfig.yml ./plugins/PetShop/config.yml
-cp bananosuiteconfig.yml ./plugins/BananoSuite/config.yml
 cp pvptoggle.yml ./plugins/PvPToggle/config.yml
 
 if [ "$CHECK_BACKUP" = true ] ; then
